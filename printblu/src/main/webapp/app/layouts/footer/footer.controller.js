a1ea0bc574edb28aae917ab6577902115a739944
harshit <harshitb95@gmail.com>
2017-11-30 00:06:28 +0530

    added footer

---
+++ printblu/src/main/webapp/app/layouts/footer/footer.controller.js
@@ -0,0 +1,14 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('printbluApp')
+        .controller('FooterController', FooterController);
+
+    FooterController.$inject = [];
+
+    function FooterController () {
+        var vm = this;
+
+    }
+})();
